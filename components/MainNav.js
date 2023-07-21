a77e527b88fd6e1263731e3350a6f34729849823
mayank-Pareek <mpareek365@gmail.com>
2023-07-21 09:56:56 -0400

    Make navbar responsive

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,29 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Form, Nav, Navbar } from "react-bootstrap";
 import Link from "next/link";
 import { useRouter } from "next/router";
 const MainNav = () => {
   const router = useRouter();
 
+  const [isExpanded, setIsExpanded] = useState(false);
   const handleSubmit = (e) => {
     e.preventDefault();
+    setIsExpanded(false);
     const searchField = e.target.elements.search.value;
     const query = `/artwork?title=true&q=${searchField}`;
     router.push(query);
   };
   return (
     <>
-      <Navbar expand="md" className="fixed-top navbar-dark bg-primary">
+      <Navbar
+        expand="md"
+        expanded={isExpanded}
+        className="fixed-top navbar-dark bg-primary"
+      >
         <Navbar.Brand>Mayank Kumar</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          onClick={() => setIsExpanded(!isExpanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
+          <Nav className="basic-navbar-nav">
             <Link href="/" passHref legacyBehavior>
-              <Nav.Link>Home</Nav.Link>
+              <Nav.Link onClick={() => setIsExpanded(false)}>Home</Nav.Link>
             </Link>
             <Link href="/search" passHref legacyBehavior>
-              <Nav.Link>Advanced Search</Nav.Link>
+              <Nav.Link onClick={() => setIsExpanded(false)}>
+                Advanced Search
+              </Nav.Link>
             </Link>
+            &nbsp;
             <Form className="d-flex nav-form" onSubmit={handleSubmit}>
               <Form.Control
                 type="search"
@@ -36,6 +48,7 @@ const MainNav = () => {
                 Search
               </Button>
             </Form>
+            &nbsp;
           </Nav>
         </Navbar.Collapse>
       </Navbar>
